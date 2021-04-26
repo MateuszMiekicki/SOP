744b2fb2d7240bd734338fa3ba72157cd5b1374f
Mateusz Miękicki <miekickimateusz@gmail.com>
2021-04-26 12:21:15 +0200

    Virtual address structure

--- 03.29/main.cc
+++ 03.29/main.cc
@@ -19,6 +19,10 @@ struct arena
         currnet += n;
         return ptr + currnet - n;
     }
+    ~arena()
+    {
+        free(ptr);
+    }
 };
 int main()
 {
@@ -33,5 +37,20 @@ int main()
         {
             std::cout << *(n + i) << '\n';
         }
+    }
+    {
+        uint32_t ptr = 0x12345678;
+
+        const uint32_t PAGE_OFFSET_MASK{0x00000FFF};
+        uint32_t pageOffset = (ptr & PAGE_OFFSET_MASK) >> 0;
+        std::cout << std::hex << pageOffset << '\n';
+
+        const uint32_t SECOND_OFFSET_MASK{0x003FF000};
+        uint32_t secondLevelPage = (ptr & SECOND_OFFSET_MASK) >> 12;
+        std::cout << std::hex << secondLevelPage << '\n';
+
+        const uint32_t FIRST_OFFSET_MASK{0xFFC00000};
+        uint32_t firstLevelPage = (ptr & FIRST_OFFSET_MASK) >> 22;
+        std::cout << std::hex << firstLevelPage << '\n';
     }
 }
